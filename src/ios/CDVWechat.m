5e6d03b80feaf7bd7b1efff8c7744a9b51c4a34b
Alben Yuan <albenyuan@aliyun.com>
2017-11-26 12:15:28 +0800

    支持图片的分享

--- src/ios/CDVWechat.m
+++ src/ios/CDVWechat.m
@@ -8,8 +8,6 @@
 
 #import "CDVWechat.h"
 
-static int const MAX_THUMBNAIL_SIZE = 320;
-
 @implementation CDVWechat
 
 #pragma mark "API"
@@ -74,7 +72,7 @@ static int const MAX_THUMBNAIL_SIZE = 320;
             req.message = [self buildSharingMessage:message];
             if (![WXApi sendReq:req])
             {
-                [self failWithCallbackID:command.callbackId withMessage:@"发送请求失败"];
+                [self failWithCallbackID:command.callbackId withMessage:@"参数错误"];
                 self.currentCallbackId = nil;
             }
         }];
@@ -86,7 +84,7 @@ static int const MAX_THUMBNAIL_SIZE = 320;
         
         if (![WXApi sendReq:req])
         {
-            [self failWithCallbackID:command.callbackId withMessage:@"发送请求失败"];
+            [self failWithCallbackID:command.callbackId withMessage:@"参数错误"];
             self.currentCallbackId = nil;
         }
     }
@@ -120,7 +118,7 @@ static int const MAX_THUMBNAIL_SIZE = 320;
     }
     else
     {
-        [self failWithCallbackID:command.callbackId withMessage:@"发送请求失败"];
+        [self failWithCallbackID:command.callbackId withMessage:@"参数错误"];
     }
 }
 
@@ -363,7 +361,6 @@ static int const MAX_THUMBNAIL_SIZE = 320;
         case CDVWXSharingTypeFile:
             mediaObject = [WXFileObject object];
             ((WXFileObject*)mediaObject).fileData = [self getNSDataFromURL:[media objectForKey:@"file"]];
-            ((WXFileObject*)mediaObject).fileExtension = [media objectForKey:@"fileExtension"];
             break;
             
         case CDVWXSharingTypeImage:
@@ -410,9 +407,10 @@ static int const MAX_THUMBNAIL_SIZE = 320;
     {
         url =  [NSTemporaryDirectory() stringByAppendingPathComponent:[url componentsSeparatedByString:@"temp:"][1]];
         data = [NSData dataWithContentsOfFile:url];
-    }
-    else
-    {
+    }else if ([url rangeOfString:@"tmp"].length!=0) {
+
+             data = [NSData dataWithContentsOfFile:url];
+    }else{
         // local file
         url = [[NSBundle mainBundle] pathForResource:[url stringByDeletingPathExtension] ofType:[url pathExtension]];
         data = [NSData dataWithContentsOfFile:url];
@@ -424,35 +422,7 @@ static int const MAX_THUMBNAIL_SIZE = 320;
 - (UIImage *)getUIImageFromURL:(NSString *)url
 {
     NSData *data = [self getNSDataFromURL:url];
-    UIImage *image = [UIImage imageWithData:data];
-    
-    if (image.size.width > MAX_THUMBNAIL_SIZE || image.size.height > MAX_THUMBNAIL_SIZE)
-    {
-        CGFloat width = 0;
-        CGFloat height = 0;
-        
-        // calculate size
-        if (image.size.width > image.size.height)
-        {
-            width = MAX_THUMBNAIL_SIZE;
-            height = width * image.size.height / image.size.width;
-        }
-        else
-        {
-            height = MAX_THUMBNAIL_SIZE;
-            width = height * image.size.width / image.size.height;
-        }
-        
-        // scale it
-        UIGraphicsBeginImageContext(CGSizeMake(width, height));
-        [image drawInRect:CGRectMake(0, 0, width, height)];
-        UIImage *scaled = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        return scaled;
-    }
-    
-    return image;
+    return [UIImage imageWithData:data];
 }
 
 - (void)successWithCallbackID:(NSString *)callbackID
